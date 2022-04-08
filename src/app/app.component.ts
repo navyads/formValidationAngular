ff34708884563cb801b7d0d5b479fa0e34b949b9
navyads <navyads22@gmail.com>
2022-04-08 06:41:35 +0000

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,36 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularapp';
+
+  regForm=new FormGroup({
+    fname:new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(50)]),
+    gender:new FormControl('',[Validators.required]),
+    country:new FormControl('',[Validators.required]),
+    city:new FormControl('',[Validators.required]),
+    age:new FormControl('')
+  })
+
+  regUser(){
+    console.log(this.regForm.value)
+  }
+
+  get fname(){
+    return this.regForm.get('fname');
+  }
+
+  get gender(){
+    return this.regForm.get('gender');
+  }
+
+  get country(){
+    return this.regForm.get('country');
+  }
+
+  get city(){
+    return this.regForm.get('city');
+  }
+
+  get age(){
+    return this.regForm.get('age');
+  }
 }
